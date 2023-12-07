aa17c3f1b972ba225e30a02c6bc47b58a088e274
EugeneyCh <eugeney@ua.fm>
2023-12-07 17:49:34 +0200

    Third_Day

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,9 +5,21 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType=""/>
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,10 +28,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -29,13 +43,20 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
-    width:50,
+    width: 50,
     fontSize: 32,
     borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
     color: "#ddb52f",
     marginVertical: 8,
     fontWeight: "bold",
-    textAlign:'center',
+    textAlign: "center",
   },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer:{
+    flex:1,
+    
+  }
 });
